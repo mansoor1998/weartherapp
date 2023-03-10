ad345955e26a4682eefc715e7540e93fcbdf7bd4
Mansoor Ali Soomro <mk.soomro@yahoo.com>
2023-03-10 10:15:19 +0100

    first commit

---
+++ Test/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string GetTestData()
+        {
+            return "This will be returning the test string";
+        }
+    }
+}
